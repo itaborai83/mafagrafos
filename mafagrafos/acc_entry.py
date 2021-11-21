fb15227b54df81b1de2664b3dab6fcb18f7f2733
Daniel Lemos Itaboraí <daniel.itaborai@petrobras.com.br>
2021-11-20 21:42:58 -0300

    remove test cases from the class itself

--- mafagrafos/acc_entry.py
+++ mafagrafos/acc_entry.py
@@ -53,45 +53,4 @@ class AccEntry:
     
     def __str__(self):
         return f"<AccEntry dst='{self.dst}', src='{self.src}', ammount={self.ammount}>"
-
-    @classmethod
-    def get_test_case_01(klass):
-        return "CJ14", [
-            AccEntry("CJ20", None,   1_000_000.00)
-        ,   AccEntry("CJ10", "CJ20",   850_000.00)
-        ,   AccEntry("CJ12", "CJ20",   150_000.00)
-        ,   AccEntry("CJ10", None,     400_000.00)
-        ,   AccEntry("CJ11", None,     200_000.00) # look here
-        ,   AccEntry("CJ11", "CJ10",   500_000.00)
-        ,   AccEntry("CJ13", "CJ10",   750_000.00)
-        ,   AccEntry("CJ13", None,     100_000.00)
-        ,   AccEntry("CJ14", "CJ13",   600_800.00)
-        ,   AccEntry("CJ11", "CJ13",   150_200.00)
-        ,   AccEntry("CJ11", None,     200_000.00)
-        ,   AccEntry("CJ14", "CJ11",   595_140.00)
-        ,   AccEntry("CJ20", "CJ11",   255_060.00)
-        ,   AccEntry("CJ12", "CJ20",   255_060.00)
-        ,   AccEntry("CJ14", None,     200_000.00)
-        ,   AccEntry("CJ14", "CJ12",   202_530.00)
-        ,   AccEntry("CJ13", "CJ11",   100_000.00)
-        ,   AccEntry("CJ14", "CJ11",   100_000.00)
-        ,   AccEntry("CJ20", "CJ13",    50_000.00)
-        ,   AccEntry("CJ10", "CJ20",    50_000.00)
-        ,   AccEntry("CJ14", "CJ10",    25_000.00)
-        ]
     
-    @classmethod
-    def get_test_case_02(klass):
-        return "X", [
-            AccEntry("C",    None,	-200)
-        ,   AccEntry("C",    None,	 300)
-        ,   AccEntry("B",    "C",    100)
-        ,   AccEntry("A",    "B",     70)
-        ,   AccEntry("C",    "B",     30)
-        ,   AccEntry("X",    "A",     60)
-        ,   AccEntry("Y",    "A",     10)
-        ,   AccEntry("B",    "C",     30)
-        ,   AccEntry("D",    "B",     30)
-        ,   AccEntry("X",    "D",     15)
-        ,   AccEntry("Z",    "D",     15)
-        ]
